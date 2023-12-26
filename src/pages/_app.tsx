a65714cdd981b3a53d742f176b4718fda501c8cf
0hyeon <djdjdjk2006@naver.com>
2023-12-27 02:20:03 +0900

    feat: husky setting

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import '@/styles/globals.css'
+import { AnimatePresence } from 'framer-motion'
+import type { AppProps } from 'next/app'
+import Link from 'next/link'
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <>
+      <div className="main">
+        <div className="header">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </div>
+      </div>
+      <AnimatePresence>
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </>
+  )
+}
