258fb237b142ed787c24aec62ef6baf576bcc8b7
0hyeon <djdjdjk2006@naver.com>
2023-12-27 18:12:50 +0900

    style: 페이지전환

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,14 @@
+// import '@/styles/styles.scss'
 import '@/styles/globals.css'
 import { AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
 import Link from 'next/link'
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <>
-      <div className="main">
-        <div className="header">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-        </div>
-      </div>
-      <AnimatePresence>
-        <Component {...pageProps} />
+    <div className="main">
+      <AnimatePresence mode="wait">
+        <Component key={router.route} {...pageProps} />
       </AnimatePresence>
-    </>
+    </div>
   )
 }
---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,28 @@
+import Inner from '@/components/Layout/Inner'
+import Stair from '@/components/Layout/Stair'
+import Head from 'next/head'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact</title>
+        <meta name="description" content="app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Stair>
+        <h1>Contact</h1>
+        <div className="body">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            inventore distinctio quia. Quam necessitatibus delectus, incidunt
+          </p>
+        </div>
+      </Stair>
+    </>
+  )
+}
+
+export default Contact
